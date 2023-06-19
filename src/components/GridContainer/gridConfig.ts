5eb28639325a544e3a47b18e265008c680b82789
Ed Opare-Aryee <edwardoparearyee@gmail.com>
2023-06-19 17:57:43 +0100

    feat(EO-36): grid system

---
+++ src/components/GridContainer/gridConfig.ts
@@ -0,0 +1,9 @@
+import { Configuration } from 'react-grid-system';
+
+const gridConfig: Configuration = {
+  breakpoints: [320, 768, 1024, 1200],
+  gutterWidth: 16,
+  maxScreenClass: 'xl',
+};
+
+export default gridConfig;
