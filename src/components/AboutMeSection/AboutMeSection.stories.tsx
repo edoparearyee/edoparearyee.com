4a44903f9b2cb70530acf9aa76b0c4e42125f8d5
Ed Opare-Aryee <edwardoparearyee@gmail.com>
2023-06-22 17:06:20 +0100

    feat(EO-38): about page assembly (#7)

---
+++ src/components/AboutMeSection/AboutMeSection.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import AboutMeSection from './AboutMeSection';
+
+const meta: Meta<typeof AboutMeSection> = {
+  title: 'Sections/AboutMe',
+  component: AboutMeSection,
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof AboutMeSection>;
+
+const defaultArgs = {};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    ...defaultArgs,
+  },
+};
