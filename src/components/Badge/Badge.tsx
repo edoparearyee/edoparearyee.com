69386359dd9f9d5f21b8ade5d96cbd8f78bcd607
Ed Opare-Aryee <edwardoparearyee@gmail.com>
2023-06-27 12:55:05 +0100

    feat(EO-43): badges for technology list (#11)

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,43 @@
+import React, { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+
+import styles from './Badge.module.scss';
+
+type BadgeColor =
+  | 'primary'
+  | 'secondary'
+  | 'warning'
+  | 'success'
+  | 'destructive';
+type BadgeAppearance = 'solid' | 'outline';
+type BadgeShape = 'square' | 'pill';
+type BadgeSize = 'small' | 'medium' | 'large';
+
+export type BadgeProps = PropsWithChildren<{
+  appearance: BadgeAppearance;
+  shape: BadgeShape;
+  color: BadgeColor;
+  size: BadgeSize;
+  className?: string;
+}>;
+
+const Badge: React.FC<BadgeProps> = ({
+  color = 'primary',
+  appearance = 'solid',
+  shape = 'square',
+  size = 'medium',
+  className,
+}) => (
+  <div
+    className={classNames(
+      styles.badge,
+      styles[`badge--variant-${appearance}-${color}-${shape}`],
+      styles[`badge--size-${size}`],
+      className,
+    )}
+  >
+    Badge works!
+  </div>
+);
+
+export default Badge;
