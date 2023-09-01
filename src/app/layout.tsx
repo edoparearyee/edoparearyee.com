bf8e00f6543ce7b958d19bd3eceebd2b221383e1
Ed Opare-Aryee <ed@edoparearyee.com>
2023-09-01 12:24:46 +0100

    feat(EO-68): favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { Metadata } from 'next';
 
 import Header from '@/components/Header/Header';
@@ -25,6 +26,27 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#12e1b9" />
+        <meta name="msapplication-TileColor" content="#12e1b9" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <Header />
