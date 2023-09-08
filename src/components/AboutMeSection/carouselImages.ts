7576514688a1f3c750ad27cd6655d123f7610526
Ed Opare-Aryee <ed@edoparearyee.com>
2023-09-08 12:07:47 +0100

    feat(EO-13): handle loading more case studies (#37)

--- src/components/AboutMeSection/carouselImages.ts
+++ src/components/AboutMeSection/carouselImages.ts
@@ -23,6 +23,16 @@ export const aboutMeCarouselImagesMobile = [
     image: [
       {
         breakpoint: 375,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1694106250/edoparearyee.com/HSBC%20Kinetic/mobile/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1694106250/edoparearyee.com/HSBC%20Kinetic/mobile/01-hero.webp',
+      },
+    ],
+    alt: '',
+  },
+  {
+    image: [
+      {
+        breakpoint: 375,
         '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1692357832/edoparearyee.com/FOX%20WWE%20Moonsault/Mobile/01-hero.webp',
         '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1692357832/edoparearyee.com/FOX%20WWE%20Moonsault/Mobile/01-hero.webp',
       },
@@ -68,6 +78,26 @@ export const aboutMeCarouselImagesDesktop = [
         breakpoint: 375,
         '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1692792807/edoparearyee.com/Beaverbrooks/desktop/03-as-chosen-by.webp',
         '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1692792807/edoparearyee.com/Beaverbrooks/desktop/03-as-chosen-by.webp',
+      },
+    ],
+    alt: '',
+  },
+  {
+    image: [
+      {
+        breakpoint: 1024,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_590,q_auto:best,w_912/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1180,q_auto:best,w_1824/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
+      },
+      {
+        breakpoint: 768,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_499,q_auto:best,w_768/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_998,q_auto:best,w_1536/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
+      },
+      {
+        breakpoint: 375,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1694106251/edoparearyee.com/HSBC%20Kinetic/desktop/01-hero.webp',
       },
     ],
     alt: '',
