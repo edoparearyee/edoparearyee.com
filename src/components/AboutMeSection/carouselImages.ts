7fe5298f923c44966f88c335a39c4a2e0ea88763
Ed Opare-Aryee <ed@edoparearyee.com>
2023-09-11 13:38:30 +0100

    feat(EO-17): add more eight road and nandos screens to about gallery

--- src/components/AboutMeSection/carouselImages.ts
+++ src/components/AboutMeSection/carouselImages.ts
@@ -3,16 +3,6 @@ export const aboutMeCarouselImagesMobile = [
     image: [
       {
         breakpoint: 375,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1691145027/edoparearyee.com/Google%20Black%20Owned%20Friday/Mobile/01-home.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1691145027/edoparearyee.com/Google%20Black%20Owned%20Friday/Mobile/01-home.webp',
-      },
-    ],
-    alt: '',
-  },
-  {
-    image: [
-      {
-        breakpoint: 375,
         '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1692792806/edoparearyee.com/Beaverbrooks/mobile/03-as-chosen-by.webp',
         '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1692792806/edoparearyee.com/Beaverbrooks/mobile/03-as-chosen-by.webp',
       },
@@ -33,8 +23,18 @@ export const aboutMeCarouselImagesMobile = [
     image: [
       {
         breakpoint: 375,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1692357832/edoparearyee.com/FOX%20WWE%20Moonsault/Mobile/01-hero.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1692357832/edoparearyee.com/FOX%20WWE%20Moonsault/Mobile/01-hero.webp',
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1694174359/edoparearyee.com/Nandos/mobile/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1694174359/edoparearyee.com/Nandos/mobile/01-hero.webp',
+      },
+    ],
+    alt: '',
+  },
+  {
+    image: [
+      {
+        breakpoint: 375,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_780,q_auto:best,w_365/v1694432064/edoparearyee.com/Eight%20Roads/mobile/01-home.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1560,q_auto:best,w_730/v1694432064/edoparearyee.com/Eight%20Roads/mobile/01-home.webp',
       },
     ],
     alt: '',
@@ -42,26 +42,6 @@ export const aboutMeCarouselImagesMobile = [
 ];
 
 export const aboutMeCarouselImagesDesktop = [
-  {
-    image: [
-      {
-        breakpoint: 1024,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_590,q_auto:best,w_912/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1180,q_auto:best,w_1824/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-      },
-      {
-        breakpoint: 768,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_499,q_auto:best,w_768/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_998,q_auto:best,w_1536/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-      },
-      {
-        breakpoint: 375,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1691145029/edoparearyee.com/Google%20Black%20Owned%20Friday/Desktop/01-home.webp',
-      },
-    ],
-    alt: '',
-  },
   {
     image: [
       {
@@ -106,18 +86,38 @@ export const aboutMeCarouselImagesDesktop = [
     image: [
       {
         breakpoint: 1024,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_590,q_auto:best,w_912/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1180,q_auto:best,w_1824/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_590,q_auto:best,w_912/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1180,q_auto:best,w_1824/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
       },
       {
         breakpoint: 768,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_499,q_auto:best,w_768/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_998,q_auto:best,w_1536/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_499,q_auto:best,w_768/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_998,q_auto:best,w_1536/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
       },
       {
         breakpoint: 375,
-        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
-        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1692357828/edoparearyee.com/FOX%20WWE%20Moonsault/Desktop/01-hero.webp',
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1694174361/edoparearyee.com/Nandos/desktop/01-hero.webp',
+      },
+    ],
+    alt: '',
+  },
+  {
+    image: [
+      {
+        breakpoint: 1024,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_590,q_auto:best,w_912/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_1180,q_auto:best,w_1824/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
+      },
+      {
+        breakpoint: 768,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_499,q_auto:best,w_768/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_998,q_auto:best,w_1536/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
+      },
+      {
+        breakpoint: 375,
+        '1x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_244,q_auto:best,w_375/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
+        '2x': 'https://res.cloudinary.com/edoparearyee/image/upload/c_fill,g_north,h_488,q_auto:best,w_750/v1694432064/edoparearyee.com/Eight%20Roads/desktop/01-home.webp',
       },
     ],
     alt: '',
