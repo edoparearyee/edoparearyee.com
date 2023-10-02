63ef45036483344c498b86c9903400ff0c2afc00
Ed Opare-Aryee <edwardoparearyee@gmail.com>
2023-10-02 11:25:34 +0100

    fix(EO-86): remove subdomain from sitemap urls

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,7 @@ import { getCaseStudies } from '@/services/contentful';
 
 export default async function sitemap() {
   const limit = 12;
+  const baseUrl = 'https://edoparearyee.com';
   let { items: caseStudies, total } = await getCaseStudies({ limit });
 
   do {
@@ -15,13 +16,13 @@ export default async function sitemap() {
 
   return [
     {
-      url: 'https://www.edoparearyee.com',
+      url: `${baseUrl}`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     ...caseStudies.map(({ slug }) => ({
-      url: `https://www.edoparearyee.com/work/${slug}`,
+      url: `${baseUrl}/work/${slug}`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
